e113b53b63cb102a6ad3b15ef73ac626c98a629e
Wladimir Bandeira <tecnico100@gmail.com>
2020-08-04 14:26:09 -0300

    construindo espolso com dados mockados

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test de Recipe',
+      'This simples teste',
+      'https://get.pxhere.com/photo/ocean-restaurant-dish-meal-food-seafood-recipe-fish-cuisine-crab-shell-lemon-asian-food-platter-clams-prawns-oysters-brunch-thai-food-animal-source-foods-southeast-asian-food-shanghai-food-portuguese-food-1376447.jpg')
+  ];
 
   constructor() { }
 
